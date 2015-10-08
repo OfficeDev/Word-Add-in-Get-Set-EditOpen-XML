3ea75c439694c302c55c530146418c0e91f6fabf
Michael Mainer <mmainer@microsoft.com>
2015-10-08 11:33:28 -0700

    updated code to use Word2016 API

--- C#/WD_OpenXML_jsWeb/App/WD_OpenXML_js/WD_OpenXML_js.js
+++ C#/WD_OpenXML_jsWeb/App/WD_OpenXML_js/WD_OpenXML_js.js
@@ -8,14 +8,147 @@
 Office.initialize = function (reason) {
     $(document).ready(function () {
 
+      // Use this to check whether the new API is supported in the new Word Javascript API..
+      if (Office.context.requirements.isSetSupported("WordApi", "1.1")) {
+        // Do something that is only available via the new APIs
+        
+        $('#getOOXMLData').click(function () { getOOXML_newAPI(); });
+        $('#setOOXMLData').click(function () { setOOXML_newAPI(); });
+        console.log('This code is using Word 2016 or greater.');
+      }
+      else {
+        // Just letting you know that this code will not work with your version of Word.
+        console.log('This code is using Word 2013.');
         // Wire up the click events of the two buttons in the WD_OpenXML_js.html page.
         $('#getOOXMLData').click(function () { getOOXML(); });
         $('#setOOXMLData').click(function () { setOOXML(); });
+
+      }
+
+
+
     });
 };
 
 // Variable to hold any Office Open XML
 var currentOOXML = "";
+
+function getOOXML_newAPI() {
+  // Get a reference to the Div where we will write the status of our operation
+  var report = document.getElementById("status");
+  var textArea = document.getElementById("dataOOXML");
+  // Remove all nodes from the status Div so we have a clean space to write to
+  while (report.hasChildNodes()) {
+    report.removeChild(report.lastChild);
+  }
+
+  // Run a batch operation against the Word object model.
+  Word.run(function (context) {
+
+    // Create a proxy object for the document body.
+    var body = context.document.body;
+
+    // Queue a commmand to get the OOXML contents of the body.
+    var bodyOOXML = body.getOoxml();
+
+    // Synchronize the document state by executing the queued commands, 
+    // and return a promise to indicate task completion.
+    return context.sync().then(function (results) {
+
+      // 
+      if (results.status === 'succeeded') {
+        currentOOXML = bodyOOXML.value;
+
+        while (textArea.hasChildNodes()) {
+          textArea.removeChild(textArea.lastChild);
+          report.innerText = "";
+        };
+        setTimeout(function () {
+          textArea.appendChild(document.createTextNode(currentOOXML));
+          report.innerText = "The getOOXML function succeeded!";
+        }, 400);
+
+        // Clear the success message after a 2 second delay
+        setTimeout(function () {
+          report.innerText = "";
+        }, 2000);
+      }
+      else {
+        currentOOXML = "";
+        report.innerText = results.error.message;
+      }
+    });
+  })
+  .catch(function (error) {
+    console.log("Error: " + JSON.stringify(error));
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+function setOOXML_newAPI() {
+  // Get a reference to the Div where we will write the outcome of our operation
+  var report = document.getElementById("status");
+
+  //Sets the currentOOXML variable to the current contents of the task pane text area
+  currentOOXML = document.getElementById("dataOOXML").textContent
+
+  // Remove all nodes from the status Div so we have a clean space to write to
+  while (report.hasChildNodes()) {
+    report.removeChild(report.lastChild);
+  }
+
+  // Check whether we have OOXML in the variable
+  if (currentOOXML != "") {
+
+    // Call the setSelectedDataAsync, with parameters of:
+    // 1. The Data to insert.
+    // 2. The coercion type for that data.
+    // 3. A callback function that lets us know if it succeeded.
+
+    // Run a batch operation against the Word object model.
+    Word.run(function (context) {
+
+      // Create a proxy object for the document body.
+      var body = context.document.body;
+
+      // Queue a commmand to insert OOXML in to the beginning of the body.
+      body.insertOoxml(currentOOXML, Word.InsertLocation.start);
+
+      // Synchronize the document state by executing the queued commands, 
+      // and return a promise to indicate task completion.
+      return context.sync().then(function (result) {
+
+        // Tell the user we succeeded and then clear the message after a 2 second delay
+        if (result.status === "succeeded") {
+          report.innerText = "The setOOXML function succeeded!";
+          setTimeout(function () {
+            report.innerText = "";
+          }, 2000);
+        }
+        else {
+          // This runs if the method does not return a success flag
+          report.innerText = result.error.message;
+
+          // Clear the text area just so we don't give you the impression that there's
+          // valid OOXML waiting to be inserted... 
+          while (textArea.hasChildNodes()) {
+            textArea.removeChild(textArea.lastChild);
+          }
+        }
+
+      });
+    })
+    .catch(function (error) {
+      console.log('Error: ' + JSON.stringify(error));
+      if (error instanceof OfficeExtension.Error) {
+        console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+      }
+    });
+
+  }
+}
 
 function getOOXML() {
     // Get a reference to the Div where we will write the status of our operation
